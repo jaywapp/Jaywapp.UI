e7a7c266dae7e8dc484c5a82c6019fcda23d3644
junyoungpark10 <jaywapp16@gmail.com>
2023-02-02 14:02:28 +0900

    Initialize

---
+++ CategoriedComboBox.xaml.cs
@@ -0,0 +1,110 @@
+﻿using Jaywapp.UI.CategoriedComboBox.Interface;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Jaywapp.UI.CategoriedComboBox
+{
+    /// <summary>
+    /// CategoriedComboBox.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class CategoriedComboBox : UserControl
+    {
+        #region Dependency Properties
+        private static FrameworkPropertyMetadata Metadata = new FrameworkPropertyMetadata(
+                null,
+                FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+                OnDependencyPropertyChanged);
+
+        public static readonly DependencyProperty CategoriedItemsSourceProperty = DependencyProperty.Register(
+            nameof(CategoriedItemsSource),
+            typeof(IEnumerable<ICategoriedItem>),
+            typeof(CategoriedComboBox),
+            Metadata);
+
+        public static readonly DependencyProperty SelectedTextProperty = DependencyProperty.Register(
+            nameof(SelectedText), 
+            typeof(string), 
+            typeof(CategoriedComboBox),
+            Metadata);
+        #endregion
+
+        #region Properties
+        public IEnumerable<ICategoriedItem> CategoriedItemsSource
+        {
+            get => (IEnumerable<ICategoriedItem>)GetValue(CategoriedItemsSourceProperty);
+            set => SetValue(CategoriedItemsSourceProperty, value);
+        }
+
+        public string SelectedText
+        {
+            get => (string)GetValue(SelectedTextProperty);
+            set => SetValue(SelectedTextProperty, value);
+        }
+        #endregion
+
+        #region Constructor
+        public CategoriedComboBox()
+        {
+            InitializeComponent();
+            _comboBox.SelectionChanged += OnSelectedItemChanged;
+        }
+        #endregion
+
+        #region Functions
+        private static void OnDependencyPropertyChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            var control = obj as CategoriedComboBox;
+
+            if (e.Property == CategoriedItemsSourceProperty)
+            {
+                control.OnCategoriedItemsSourcePropertyChanged(e);
+            }
+            else if (e.Property == SelectedTextProperty)
+            {
+                control.OnSelectedTextPropertyChanged(e);
+            }
+        }
+
+        private void OnSelectedTextPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            var text = e.NewValue as string;
+            var target = CategoriedItemsSource.FirstOrDefault(s => s.Text == text);
+            if (target == null)
+                return;
+
+            _comboBox.SelectedItem = target;
+        }
+
+        private void OnCategoriedItemsSourcePropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            var items = e.NewValue as IEnumerable<ICategoriedItem>;
+            if (items == null)
+                return;
+
+            _comboBox.ItemsSource = CreateListCollectionView(items);
+        }
+
+        private void OnSelectedItemChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var count = e.AddedItems.Count;
+            var selected = e.AddedItems[count - 1];
+
+            if (selected is ICategoriedItem item)
+                SelectedText = item.Text;
+        }
+
+        private static ListCollectionView CreateListCollectionView(IEnumerable<ICategoriedItem> items)
+        {
+            var collectionView = new ListCollectionView(items.ToList());
+            var groupDescription = new PropertyGroupDescription(nameof(ICategoriedItem.Category));
+
+            collectionView.GroupDescriptions.Add(groupDescription);
+
+            return collectionView;
+        }
+        #endregion
+    }
+}
---
+++ Interface/ICategoriedItem.cs
@@ -0,0 +1,19 @@
+﻿namespace Jaywapp.UI.CategoriedComboBox.Interface
+{
+    /// <summary>
+    /// Model including category information by using for combo box "ItemsSource"
+    /// </summary>
+    public interface ICategoriedItem
+    {
+        /// <summary>
+        /// Category Information
+        /// It will be used when grouping.
+        /// </summary>
+        string Category { get; }
+
+        /// <summary>
+        /// Combobox real value
+        /// </summary>
+        string Text { get; }
+    }
+}
