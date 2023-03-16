373953b43d26bfa3176bbf9865eeeff7b5c90a5b
junyoungpark10 <junyoung.park@penta-cube.com>
2023-03-16 09:09:41 +0900

    add block view

---
+++ Block/BlockView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Jaywapp.Infrastructure.Block.Interface;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Jaywapp.UI.Block
+{
+    /// <summary>
+    /// BlockView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class BlockView : UserControl
+    {
+        public static readonly DependencyProperty BlockProperty = DependencyProperty.Register(
+            nameof(Block), typeof(IBlock), typeof(BlockView), new FrameworkPropertyMetadata(null));
+
+        public IBlock Block
+        {
+            get => (IBlock)GetValue(BlockProperty);
+            set => SetValue(BlockProperty, value);
+        }
+
+        public BlockView()
+        {
+            InitializeComponent();
+        }
+    }
+}
